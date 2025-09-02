a4c73f6188b7cf350c5a99331fc7d61679ad2ea7
Max <mc.cui@mail.utoronto.ca>
2025-09-01 21:39:13 -0700

    asou

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def reset_game():
     return jsonify({'word': 'ARISE'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=7860)
